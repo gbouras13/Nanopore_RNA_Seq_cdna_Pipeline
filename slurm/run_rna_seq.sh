0744d568a864b15a475198e5b40ab9c41f8de59f
gbouras13 <84495559+gbouras13@users.noreply.github.com>
2022-02-22 10:22:58 +1030

    slurn

---
+++ slurm/run_rna_seq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=ghais_rna_seq
+#SBATCH --mail-user=george.bouras@adelaide.edu.au
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --err="ghais_rna_seq_snk.err"
+#SBATCH --output="ghais_rna_seq_snk.out"
+
+# Resources allocation request parameters
+#SBATCH -p batch
+#SBATCH -N 1                                                    # number of tasks (sequential job starts 1 task) (check this if your job unexpectedly uses 2 nodes)
+#SBATCH -c 1                                                    # number of cores (sequential job calls a multi-thread program that uses 8 cores)
+#SBATCH --time=23:00:00                                         # time allocation, which has the format (D-HH:MM), here set to 1 hou                                           # generic resource required (here requires 1 GPUs)
+#SBATCH --mem=10GB                                              # specify memory required per node
+
+
+SNK_DIR="/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/Nanopore_RNA_Seq_cdna_Pipeline"
+PROF_DIR="/hpcfs/users/a1667917/snakemake_slurm_profile"
+
+cd $SNK_DIR
+
+module load Anaconda3/2020.07
+conda activate snakemake_clean_env
+
+snakemake -s rna_seq_runner.smk --use-conda --config Reads=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/Nanopore_RNA_Seq_cdna_Pipeline/fastqs  Output=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/RNA_Seq_Pipeline_Out  --profile wgs_tcga
+
+conda deactivate
