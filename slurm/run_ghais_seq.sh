9ece2ff42298883df8f59490b6c534187ee69591
gbouras13 <george.bouras@adelaide.edu.au>
2022-09-26 18:14:55 +0930

    fix slurm

--- slurm/run_ghais_seq.sh
+++ slurm/run_ghais_seq.sh
@@ -24,6 +24,7 @@ cd $SNK_DIR
 module load Anaconda3/2020.07
 conda activate snakemake_clean_env
 
-snakemake -s rna_seq_runner.smk --use-conda --config Reads=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/Nanopore_RNA_Seq_cdna_Pipeline/fastqs  Output=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/RNA_Seq_Pipeline_Out  --profile wgs_tcga
+snakemake -s rna_seq_runner.smk --use-conda \
+--config Reads=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/aggregated_fastqs  Output=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/RNA_Seq_Pipeline_Out  --profile wgs_tcga
 
 conda deactivate
