7f6845ec39315b399285818c0661c195f03c38d5
gbouras13 <george.bouras@adelaide.edu.au>
2022-09-26 18:17:40 +0930

    prof

--- slurm/run_ghais_seq.sh
+++ slurm/run_ghais_seq.sh
@@ -25,6 +25,6 @@ module load Anaconda3/2020.07
 conda activate snakemake_clean_env
 
 snakemake -s rna_seq_runner.smk --use-conda \
---config Reads=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/aggregated_fastqs  Output=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/RNA_Seq_Pipeline_Out  --profile wgs_tcga
+--config Reads=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/aggregated_fastqs  Output=/hpcfs/users/a1667917/Ghais/Rat_RNA_Seq/RNA_Seq_Pipeline_Out  --profile $PROF_DIR/wgs_tcga
 
 conda deactivate
